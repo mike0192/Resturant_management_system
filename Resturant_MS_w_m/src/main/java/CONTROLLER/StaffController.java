4ca7235b93b1ac16e3d1be0798bfb35dc84a25f6
Michael Hagos <michaelhagos163@gmail.com>
2026-06-28 13:31:42 +0300

    Added event handler for search, a method to clear the field, show alert and show success

--- Resturant_MS_w_m/src/main/java/CONTROLLER/StaffController.java
+++ Resturant_MS_w_m/src/main/java/CONTROLLER/StaffController.java
@@ -16,5 +16,67 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 public class StaffController {
+    @FXML private TextField IDField;
+    @FXML private TextField NameField;
+    @FXML private TextField RoleField;
+    @FXML private TextField PhoneField;
+    @FXML private TextField DateField;
+    @FXML private Button AddButton;
+    @FXML private Button UpdateButton;
+    @FXML private Button DeleteButton;
+    @FXML private Button SearchButton;
 
-}
+    StaffImp dao = new StaffImp();
+
+    @FXML
+    public void initialize(){}
+
+    @FXML
+    private void handleSearch(ActionEvent event){
+        String input = IDField.getText().trim();
+
+        if(input.isEmpty()) {
+            ShowAlert("Input Error", "Please enter a staff id to search! ");
+            return;
+
+            Staff staff = dao.searchstaffById(input);
+
+            if (staff != null) {
+                IDField.setText(staff.getId());
+                NameField.setText(staff.getName());
+                RoleField.setText(staff.getRole());
+                PhoneField.setText(String.valueOf(staff.getPhone()));
+                DateField.setText(String.valueOf(staff.getDate()));
+
+
+            } else {
+                ShowAlert("Not Found", "No staff found with this ID: " + input);
+                clearFields();
+            }
+
+        }
+
+        }
+    private void clearFields() {
+        IDField.clear();
+        NameField.clear();
+        RoleField.clear();
+        PhoneField.clear();
+        DateField.clear();
+    }
+    private void ShowAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("❌ Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void ShowSuccess(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("✅ Success");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    }
